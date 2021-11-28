127ffd6090b9d106280c5f84abfc2f1b710c529e
soumya <kharyasoumya2011@gmail.com>
2021-11-28 23:04:13 +0100

    commit

--- Support Admin/src/com/supportAdmin/service/CredentialsService.java
+++ Support Admin/src/com/supportAdmin/service/CredentialsService.java
@@ -1,42 +1,34 @@
 package com.supportAdmin.service;
+
 import java.util.Random;
 import com.supportAdmin.model.*;
+
 //Service class for generating email,password and show password
 public class CredentialsService {
-	
-	//Method to generate Password using Random
-	public static char[] generatePassword(int length) {
-	      String capitalCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	      String lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
-	      String specialCharacters = "!@#$*%#()";
-	      String numbers = "1234567890";
-	      String combinedChars = capitalCaseLetters + lowerCaseLetters + specialCharacters + numbers;
-	      Random random = new Random();
-	      char[] password = new char[length];
 
-	      password[0] = lowerCaseLetters.charAt(random.nextInt(lowerCaseLetters.length()));
-	      password[1] = capitalCaseLetters.charAt(random.nextInt(capitalCaseLetters.length()));
-	      password[2] = specialCharacters.charAt(random.nextInt(specialCharacters.length()));
-	      password[3] = numbers.charAt(random.nextInt(numbers.length()));
-	   
-	      for(int i = 0; i< 8 ; i++) {
-	         password[i] = combinedChars.charAt(random.nextInt(combinedChars.length()));
-	      }
-	      return password;
-	   }	 
-	//Method for generating email
-	 public String generateEmailAddress(String fn, String ln, String dept)
-	 {
-		 return fn+ln+"@"+dept+".abc.com";
-	 }
-	 
-	 //method for showing Credential to user
-	 public void showCredentials(String fn,String email,char[] password)
-	 {
-		 System.out.println("Dear "+fn+ " your generated credentials are following - ");
-		 System.out.println("Email---->" +email);
-	     char[] pwd= generatePassword(8);
-		 System.out.println("Password---> "+password);
-	 }
+	public static String generateRandomPassword(int len) {
+		String capitalCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		String lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+		String specialCharacters = "!@#$*%#()-_<>/";
+		String numbers = "1234567890";
+		String combinedChars = capitalCaseLetters + lowerCaseLetters + numbers + specialCharacters;
+		Random random = new Random();
+		StringBuilder sb = new StringBuilder(len);
+		for (int i = 0; i < 8; i++)
+			sb.append(combinedChars.charAt(random.nextInt(combinedChars.length())));
+		return sb.toString();
+	}
+
+	public String generateEmailAddress(String fn, String ln, String dept) {
+		return fn + ln + "@" + dept + ".abc.com";
+	}
+
+	// method for showing Credential to user
+	public void showCredentials(String fn, String email, String password) {
+		System.out.println("Dear " + fn + " your generated credentials are following - ");
+		System.out.println("Email---->" + email);
+		// char[] pwd= generateRandomPassword(8);
+		System.out.println("Password---> " + generateRandomPassword(8));
+	}
 
 }
